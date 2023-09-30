da56d5cb1ac3b834abed570716d87efe58f27f05
Ebooth <pauldejean01@gmail.com>
2023-09-30 17:42:43 +0200

    Implemented option -l to count the number of lines

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::fmt;
 use std::fs;
 
 #[derive(Parser, Debug)]
@@ -11,13 +12,53 @@ struct Args {
 
     #[arg(short)]
     c: bool,
+
+    #[arg(short)]
+    l: bool,
+}
+
+struct WcResult {
+    file: String,
+    bytes: Option<usize>,
+    lines: Option<usize>,
+}
+
+impl WcResult {
+    fn new(file: String) -> WcResult {
+        WcResult {
+            file,
+            bytes: None,
+            lines: None,
+        }
+    }
+}
+
+impl fmt::Display for WcResult {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fields = [self.bytes, self.lines]
+            .iter()
+            .filter_map(|&x| x)
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>();
+        write!(f, "{} {}", fields.join(" "), self.file)
+    }
 }
 
 fn main() {
     let args = Args::parse();
     let file = &args.file;
+
+    let mut wc_result = WcResult::new(file.to_owned());
+
     let content = fs::read(file).expect(format!("{} not found ", file).as_str());
     if args.c {
-        println!("{} {}", content.len(), file);
+        wc_result.bytes = Some(content.len());
+    };
+    let string_content = String::from_utf8(content).unwrap();
+    let lines = string_content.split("\n").collect::<Vec<_>>();
+    if args.l {
+        wc_result.lines = Some(lines.len());
     }
+
+    println!("{}", wc_result);
 }
