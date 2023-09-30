3349c60bd8cd1f35b2a70b7f45b5e5be1a422423
Ebooth <pauldejean01@gmail.com>
2023-09-30 17:17:04 +0200

    Implemented option -c to count the number of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use clap::Parser;
+use std::fs;
+
+#[derive(Parser, Debug)]
+#[command(name = "cwc")]
+#[command(author = "Ebooth <pauldejendev@gmail.com>")]
+#[command(version = "1.0")]
+#[command(about = "A copy of unix command line tool wc ", long_about = None)]
+struct Args {
+    file: String,
+
+    #[arg(short)]
+    c: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let file = &args.file;
+    let content = fs::read(file).expect(format!("{} not found ", file).as_str());
+    if args.c {
+        println!("{} {}", content.len(), file);
+    }
+}
